00a2d457f74ddf2f893cdf317d7ee67d729641e4
HonKerDyanmo <honerdynamo@gmail.com>
2023-09-21 16:11:52 +0900

    update flies

---
+++ 2023-09-21/test.py
@@ -0,0 +1,16 @@
+numbers = 100
+numbers += 100
+print(numbers)
+
+numbers += 20
+print(numbers)
+print("numbers:",numbers)
+
+string = "안녕"
+string += "!"
+string += "!"
+print("string",string)
+
+string = input("안녕하세요 입력게하세요>>")
+print(string)
+print(type(string))
---
+++ 2023-09-21/test1.py
@@ -0,0 +1,4 @@
+raw_input = int(input("inch 단의의 숫자를 입력하세요\n"))
+inch = int(raw_input)
+cm = raw_input*2.54
+print(raw_input,"inch는 cm 단위로",cm,"cm 입니다.")
