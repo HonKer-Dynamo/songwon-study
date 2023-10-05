5b58bd4b6f8d084f00db2d33771bf5b6b366d46b
HonKerDyanmo <honerdynamo@gmail.com>
2023-10-05 16:14:24 +0900

    update flies

---
+++ 2023-10-05/test.py
@@ -0,0 +1,6 @@
+format_a = "{}만원".format(5000)
+print(format_a)
+print(type(format_a))
+format_b = "{}\n{}\n{}\n".format(3000,4000,5000)
+print(format_b)
+
---
+++ 2023-10-05/test2.py
@@ -0,0 +1,9 @@
+output_a = "{:f}".format(1000)
+output_b = "{:15f}".format(1000)
+output_c = "{:+15f}".format(1000)
+output_d = "{:+015f}".format(1000)
+
+print(output_a)
+print(output_b)
+print(output_c)
+print(output_d)
